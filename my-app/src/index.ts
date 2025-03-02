c33ef17d82cc8d7b9f7e1d3e1894e622f2d83e23
Tamal <ckbtamaldipnew@gmail.com>
2025-03-03 02:16:14 +0600

    wrangler dev run issue solved

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -3,8 +3,6 @@ console.log("Hi man");
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		console.log("Hola! Soy dora");
-		
-		return new Response('Hello World!');
+		return new Response('Hello World from docker container!');
 	},
 } satisfies ExportedHandler<Env>;
