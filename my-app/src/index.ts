09b6d8bfb91d54a9ff257c80ae0ad940c3c599c2
Tamal <ckbtamaldipnew@gmail.com>
2025-03-03 01:14:57 +0600

    nothing is still working

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,5 +1,10 @@
+console.log("Hi man");
+
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		console.log("Hola! Soy dora");
+		
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
