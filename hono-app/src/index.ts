f330274dea79bdfea52767e071e3908d711cd0a7
Tamal <ckbtamaldipnew@gmail.com>
2025-03-06 00:10:03 +0600

    add database table

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -3,7 +3,9 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    msg:"This is going from a damnn hono server"
+  })
 })
 
 export default app
